f54a4a7dc06eec4fae84bbdeba646c6fcd29878d
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2021-09-25 20:12:58 +0300

    Added course table

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, admin BOOLEAN);
 
-CREATE TABLE courses (id SERIAL PRIMARY KEY, name TEXT)
+CREATE TABLE courses (id SERIAL PRIMARY KEY, name TEXT);
 
+CREATE TABLE course (id SERIAL PRIMARY KEY, course_id INTEGER REFERENCES courses, user_id INTEGER REFERENCES users);

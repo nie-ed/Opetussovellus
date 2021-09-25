5513b753a1331f0140cd2833fd68869640deb4e9
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2021-09-25 20:24:00 +0300

    Started building fuctions to check if user attending or sing up to a course

--- modify_courses.py
+++ modify_courses.py
@@ -14,3 +14,16 @@ def add_course(name):
 	db.session.execute(sql, {"name":name})
 	db.session.commit()
 
+def attending(id, user_id):
+	sql = "SELECT * FROM course WHERE user_id=:user_id"
+	result = db.session.execute(sql, {"user_id":user_id})
+	attending = result.fetchone()
+	if not attending:
+		return False
+	else:
+		return True
+
+def attend_course(user_id, course_id):
+	sql = "INSERT INTO course (user_id, course_id) VALUES (:user_id, :course_id)"
+	db.session.execute(sql, {"user_id":user_id, "course_id":course_id})
+	db.session.commit()

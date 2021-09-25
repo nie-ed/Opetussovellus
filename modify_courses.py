d4227c59db10c8c4b4c19838912814dc7234f67f
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2021-09-25 16:56:47 +0300

    Added file to modify_courses.py file, deleted courses.py file

--- modify_courses.py
+++ modify_courses.py
@@ -1,6 +1,7 @@
 from db import db
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask import session
+import routes
 
 def all_courses():
 	sql = "SELECT * FROM courses"
@@ -8,7 +9,7 @@ def all_courses():
 	all = result.fetchall()
 	return all
 
-def add_cource(name):
+def add_course(name):
 	sql = "INSERT INTO courses (name) VALUES (:name)"
 	db.session.execute(sql, {"name":name})
 	db.session.commit()

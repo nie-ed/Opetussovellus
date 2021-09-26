39b5e64c8d1af36662b2fb81127281e25b4e2e4b
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2021-09-26 11:21:45 +0300

    Made username and course name UNIQUE

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, admin BOOLEAN);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, admin BOOLEAN);
 
-CREATE TABLE courses (id SERIAL PRIMARY KEY, name TEXT);
+CREATE TABLE courses (id SERIAL PRIMARY KEY, name TEXT UNIQUE);
 
 CREATE TABLE course (id SERIAL PRIMARY KEY, course_id INTEGER REFERENCES courses, user_id INTEGER REFERENCES users);

ee6d41099d2bcc1b86100cce219583e16818a515
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2021-10-03 09:56:24 +0300

    Fixed is_admin() function. Now can see, if admin is True of False

--- users.py
+++ users.py
@@ -11,7 +11,6 @@ def get_user(username, password):
 	else:
 		hash_value = user.password
 		if check_password_hash(hash_value, password):
-			session["user_id"] = user.id
 			return user.id
 		else:
 			return False
@@ -28,23 +27,23 @@ def create_user(username, hash_value):
 	else:
 		return True
 		
-def is_admin(id):
-	sql = "SELECT admin FROM users WHERE id=:id"
-	result = db.session.execute(sql, {"id":id})
-	admin = result.fetchone()
-	return admin
+def is_admin(user_id):
+	sql = "SELECT * FROM users WHERE id=:id"
+	result = db.session.execute(sql, {"id":user_id})
+	user = result.fetchone()
+	return user.admin
 
-def is_user():
-	sql= "SELECT * FROM users WHERE username=:username"
-	result = db.session.execute(sql, {"username":session["username"]})
+def is_user(user_id):
+	sql= "SELECT * FROM users WHERE id=:id"
+	result = db.session.execute(sql, {"id":user_id})
 	user = result.fetchone()
 	if user:
 		return True
 	else:
 		return False
 
-def user_id():
-	sql= "SELECT id FROM users WHERE username=:username"
-	result = db.session.execute(sql, {"username":session["username"]})
+def user_id(user_id):
+	sql= "SELECT id FROM users WHERE id=:id"
+	result = db.session.execute(sql, {"id":user_id})
 	id_user = result.fetchone()
 	return id_user

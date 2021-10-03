e126ab7cc2a4bc2a20d6979bb39941230b5951ac
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2021-10-03 09:55:22 +0300

    Added students_in_course table, deleted course table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, admin BOOLEAN);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, admin BOOLEAN NOT NULL);
 
 CREATE TABLE courses (id SERIAL PRIMARY KEY, name TEXT UNIQUE);
 
-CREATE TABLE course (id SERIAL PRIMARY KEY, course_id INTEGER REFERENCES courses, user_id INTEGER REFERENCES users);
+CREATE TABLE students_in_course (course_id INTEGER REFERENCES courses, user_id INTEGER REFERENCES users)
+

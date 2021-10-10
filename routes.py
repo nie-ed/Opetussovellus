8f7b5a9da1913114fe62e4b397035035dafcfb7f
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2021-10-10 12:36:30 +0300

    Added fuctions for admin to modify course: add text question or text content. Added fuction for users to answer questions

--- routes.py
+++ routes.py
@@ -5,7 +5,9 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import users
 from flask import Flask
 import modify_courses
-
+import modify_tasks
+import answers
+import modify_text
 
 app.secret_key = getenv("SECRET_KEY")
 
@@ -102,10 +104,21 @@ def add_course():
 def course_site(id):
 	user_id = session["user_id"]
 	admin = users.is_admin(user_id)
+	course = modify_courses.get_course(id)
+	tasks=modify_tasks.get_all(id)
+	content = modify_text.get_all(id)
+	get_answers = answers.get_all(user_id)
+	answered = []
+	unanswered = []
+	for task in tasks:
+		if answers.find_answer(task.id, user_id):
+			answered.append(task)
+		else:
+			unanswered.append(task)
 	if request.method == "GET":
 		attending = modify_courses.attending(id, user_id)
 		if attending or admin:
-			return render_template("course.html", admin=admin)
+			return render_template("course.html", admin=admin, id = id, unanswered=unanswered, answers = get_answers, content = content, course = course)
 		else:
 			course = modify_courses.get_course(id)
 			return render_template("attend_course.html", course = course)
@@ -113,11 +126,50 @@ def course_site(id):
 		modify_courses.attend_course(id, user_id)
 		attending = modify_courses.attending(id, user_id)
 		if attending:
-			return render_template("course.html", admin=admin)
+			return render_template("course.html", admin=admin, id = id, unanswered=unanswered, answers = get_answers, content = content, course = course)
 		else:
 			return render_template("error.html", error="Kurssille ilmottautuminen ei onnistunut")
 
 
-@app.route("/modify_course")
+@app.route("/modify_course", methods=["POST"])
 def modify_course():
-	return render_template("modify_course.html")
+	if request.method == "POST":
+		id = request.form["id"]
+		return render_template("modify_course.html", id = id)
+
+#@app.route("/new_question", methods=["GET", "POST"])
+#def add_question():
+#	if request.method == "GET":
+#		return redirect("/modify_course")
+#	if request.method == "POST":
+#		course_id = request.form["id"]
+#		return render_template("add_question.html", id = course_id)
+
+
+@app.route("/create_new_question", methods=["POST"])
+def create_new_question():
+	if request.method == "POST":
+		course_id = request.form["id"]
+		text_question = request.form["text_question"]
+		modify_tasks.add_task(text_question, course_id)
+		return redirect("/courses/" + str(course_id))
+
+
+@app.route("/create_new_content", methods=["POST"])
+def create_new_content():
+	if request.method == "POST":
+		course_id = request.form["id"]
+		topic = request.form["topic"]
+		content = request.form["content"]
+		modify_text.add_text(course_id, topic, content)
+	return redirect("/courses/" + str(course_id))
+
+@app.route("/add_answer", methods=["POST"])
+def add_answer():
+	if request.method == "POST":
+		task_id = request.form["task_id"]
+		student_id = session["user_id"]
+		content = request.form["content"]
+		task_topic = request.form["topic"]
+		answers.add_answer(task_id, task_topic, student_id, content)
+		return redirect("/courses")

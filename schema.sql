db1127dff8b02a22d613842f5942dcfea5b9c9e8
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2021-10-10 12:44:33 +0300

    Created tasks, answers and course_text tables

--- schema.sql
+++ schema.sql
@@ -2,5 +2,10 @@ CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, 
 
 CREATE TABLE courses (id SERIAL PRIMARY KEY, name TEXT UNIQUE);
 
-CREATE TABLE students_in_course (course_id INTEGER REFERENCES courses, user_id INTEGER REFERENCES users)
+CREATE TABLE students_in_course (course_id INTEGER REFERENCES courses, user_id INTEGER REFERENCES users);
 
+CREATE TABLE tasks (id SERIAL PRIMARY KEY, text_question TEXT, course_id INTEGER REFERENCES courses);
+
+CREATE TABLE answers (id SERIAL PRIMARY KEY, task_id INTEGER REFERENCES tasks, task_topic TEXT, student_id INTEGER REFERENCES users, sent_at TIMESTAMP, content TEXT);
+
+CREATE TABLE course_text (id SERIAL PRIMARY KEY, topic TEXT, course_id INTEGER REFERENCES courses, content TEXT)

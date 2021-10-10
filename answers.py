8645b3985073a903d723ad1f74c188d9371375b3
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2021-10-10 17:20:53 +0300

    Modified find_answer method to bring answer for spesific task, student and course

--- answers.py
+++ answers.py
@@ -4,17 +4,17 @@ from flask import session
 import routes
 
 
-def add_answer(task_id, task_topic, student_id, content):
-	sql = "INSERT INTO answers (task_id, task_topic, student_id, content, sent_at) VALUES (:task_id, :task_topic, :student_id, :content, NOW())"
-	db.session.execute(sql, {"task_id":task_id, "task_topic":task_topic, "student_id":student_id, "content":content})
+def add_answer(task_id, task_topic, student_id, content, course_id):
+	sql = "INSERT INTO answers (task_id, task_topic, student_id, content, course_id, sent_at) VALUES (:task_id, :task_topic, :student_id, :content, :course_id, NOW())"
+	db.session.execute(sql, {"task_id":task_id, "task_topic":task_topic, "student_id":student_id, "content":content, "course_id":course_id})
 	db.session.commit()
 
-def find_answer(task_id, student_id):
-	sql = "SELECT * FROM answers WHERE task_id=:task_id and student_id=:student_id"
-	result = db.session.execute(sql, {"task_id":task_id, "student_id":student_id})
+def find_answer(task_id, student_id, course_id):
+	sql = "SELECT * FROM answers WHERE task_id=:task_id and student_id=:student_id and course_id=:course_id"
+	result = db.session.execute(sql, {"task_id":task_id, "student_id":student_id, "course_id":course_id})
 	found = result.fetchone()
 	if found:
-		return True
+		return found
 	else:
 		return False
 def get_all(student_id):
@@ -22,3 +22,5 @@ def get_all(student_id):
 	result = db.session.execute(sql, {"student_id":student_id})
 	all = result.fetchall()
 	return all
+
+

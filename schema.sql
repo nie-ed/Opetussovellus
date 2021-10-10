8d529acbbad07d437f99fce6a391440ab67acd4f
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2021-10-10 17:26:45 +0300

    Added course_id to answers table

--- schema.sql
+++ schema.sql
@@ -6,6 +6,6 @@ CREATE TABLE students_in_course (course_id INTEGER REFERENCES courses, user_id I
 
 CREATE TABLE tasks (id SERIAL PRIMARY KEY, text_question TEXT, course_id INTEGER REFERENCES courses);
 
-CREATE TABLE answers (id SERIAL PRIMARY KEY, task_id INTEGER REFERENCES tasks, task_topic TEXT, student_id INTEGER REFERENCES users, sent_at TIMESTAMP, content TEXT);
+CREATE TABLE answers (id SERIAL PRIMARY KEY, task_id INTEGER REFERENCES tasks, task_topic TEXT, student_id INTEGER REFERENCES users, sent_at TIMESTAMP, content TEXT, course_id INTEGER REFERENCES courses);
 
 CREATE TABLE course_text (id SERIAL PRIMARY KEY, topic TEXT, course_id INTEGER REFERENCES courses, content TEXT)

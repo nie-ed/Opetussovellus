6115014e326fb4560df5c51791c5349039b28f3d
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2021-10-23 23:57:11 +0300

    Minor changes

--- modify_tasks.py
+++ modify_tasks.py
@@ -4,10 +4,12 @@ from flask import session
 import routes
 
 
-def add_task(text_question, course_id):
-	sql = "INSERT INTO tasks (text_question, course_id) VALUES (:text_question, :course_id)"	
-	db.session.execute(sql, {"text_question":text_question, "course_id":course_id})
+def add_task(text_question, course_id, multiple_choice):
+	sql = "INSERT INTO tasks (text_question, course_id, multiple_choice) VALUES (:text_question, :course_id, :multiple_choice) RETURNING id"	
+	result = db.session.execute(sql, {"text_question":text_question, "course_id":course_id, "multiple_choice":multiple_choice})
+	task_id = result.fetchone()[0]
 	db.session.commit()
+	return task_id
 
 def get_task(id):
 	sql = "SELECT * FROM tasks WHERE id=:id"

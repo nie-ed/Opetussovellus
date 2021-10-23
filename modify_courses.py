b9bb8295ce47e68e0435aaabed661fbba1d6aa09
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2021-10-23 23:44:15 +0300

    Added functions to delete course, get course creator and see attending

--- modify_courses.py
+++ modify_courses.py
@@ -15,9 +15,9 @@ def get_course(course_id):
 	course = result.fetchone()
 	return course
 
-def add_course(name):
-	sql = "INSERT INTO courses (name) VALUES (:name)"
-	db.session.execute(sql, {"name":name})
+def add_course(name, course_creator):
+	sql = "INSERT INTO courses (name, course_creator) VALUES (:name, :course_creator)"
+	db.session.execute(sql, {"name":name, "course_creator":course_creator})
 	db.session.commit()
 	sql_2 = "SELECT * FROM courses WHERE name=:name"
 	result = db.session.execute(sql_2, {"name":name})
@@ -35,6 +35,12 @@ def attending(id, user_id):
 	else:
 		return True
 
+def all_students_in_course(course_id):
+	sql = "SELECT users.username FROM students_in_course, users  WHERE students_in_course.course_id=:course_id AND students_in_course.user_id = users.id"
+	result = db.session.execute(sql, {"course_id":course_id})
+	students = result.fetchall()
+	return students
+
 def attend_course(id, user_id):
 	sql = "INSERT INTO students_in_course (user_id, course_id) VALUES (:user_id, :course_id)"
 	db.session.execute(sql, {"user_id":user_id, "course_id":id})
@@ -42,6 +48,17 @@ def attend_course(id, user_id):
 
 def user_attends(user_id):
 	sql = "SELECT * FROM students_in_course WHERE user_id=:user_id "
+	result = db.session.execute(sql, {"user_id":user_id})
+	courses = result.fetchall()
+	return courses
+
+def delete_course(id):
+	sql = "DELETE FROM courses WHERE id=:id"
+	result = db.session.execute(sql, {"id":id})
+	db.session.commit()
+
+def get_courses_user_created(user_id):
+	sql = "SELECT id, name FROM courses WHERE course_creator=:user_id"
 	result = db.session.execute(sql, {"user_id":user_id})
 	courses = result.fetchall()
 	return courses

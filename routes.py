500e61f8f3eb39b4454506bbebf0d1d78334072c
Nicolette Edevbaro <nicolette.edevbaro@hotmail.com>
2021-10-24 14:50:39 +0300

    Added function to show task done bar. Fixed functions to work with new schema

--- routes.py
+++ routes.py
@@ -45,20 +45,24 @@ def create_account():
 		return render_template("signup.html")
 	if request.method == "POST":
 		username = request.form["username"]
-		password = request.form["password"]
-		password2 = request.form["password2"]
-		if not password == password2:
-			return render_template("error.html", error="Typed in two different passwords")
-		if len(username) > 30 or len(username) < 1:
-			return render_template("error.html", error="Nimi on liian pitkä tai lyhyt")
-		if len(password) > 50 or len(password) < 4:
-			return render_template("error.html", error="Salasana on liian pitkä tai lyhyt")
+		exists = users.check_if_already_user(username)
+		if exists:
+			return render_template("error.html", error="Username is already in use")
 		else:
-			hash_value = generate_password_hash(password)
-			if users.create_user(username, hash_value):
-				return redirect("/")
+			password = request.form["password"]
+			password2 = request.form["password2"]
+			if not password == password2:
+				return render_template("error.html", error="Typed in two different passwords")
+			if len(username) > 30 or len(username) < 1:
+				return render_template("error.html", error="Nimi on liian pitkä tai lyhyt")
+			if len(password) > 50 or len(password) < 4:
+				return render_template("error.html", error="Salasana on liian pitkä tai lyhyt")
 			else:
-				return render_template("error.html", error="Creating account was unsuccessfull")
+				hash_value = generate_password_hash(password)
+				if users.create_user(username, hash_value):
+					return redirect("/")
+				else:
+					return render_template("error.html", error="Creating account was unsuccessfull")
 			
 @app.route("/profile")
 def profile_route():
@@ -69,11 +73,12 @@ def profile_route():
 def profile(id):
 	if request.method == "GET":
 		user_id = session["user_id"]
+		user = users.is_user(user_id)
 		allow = False
 		admin = users.is_admin(user_id)
 		if users.is_admin(user_id):
 			allow = True
-		if users.is_user(user_id) and user_id == id:
+		if user and user_id == id:
 			allow = True
 		if not allow:
 			return render_template("error.html", error="Ei oikeutta nähdä sivua")
@@ -83,7 +88,7 @@ def profile(id):
 			courses_created = modify_courses.get_courses_user_created(session["user_id"])
 			for course in courses_attending:
 				students_courses.append(modify_courses.get_course(course.course_id))
-			return render_template("profile.html", students_courses=students_courses, admin =admin, courses_created = courses_created)
+			return render_template("profile.html", students_courses=students_courses, admin =admin, courses_created = courses_created, user = user)
 		
 		
 @app.route("/logout")
@@ -109,6 +114,7 @@ def add_course():
 			return render_template("error.html", error="Ei oikeutta nähdä sivua")
 		if allow:
 			return render_template("add_course.html")
+
 	if request.method == "POST":
 		if session["csrf_token"] != request.form["csrf_token"]:
 			abort(403)
@@ -128,16 +134,18 @@ def course_site(id):
 	course = modify_courses.get_course(id)
 	tasks=modify_tasks.get_all(id)
 	content = modify_text.get_all(id)
-	tasks=modify_tasks.get_all(id)
 	amount_tasks = len(tasks)
 	amount_answered = 0
 	for task in tasks:
-		amount_answered = amount_answered + answers.count_student_answered(task.id, user_id)
-
+		if task.multiple_choice == False:
+			amount_answered = amount_answered + answers.count_student_answered(task.id, user_id)
+		else:
+			amount_answered = amount_answered + answers.count_student_choice_answered(task.id, user_id)
+	
 	if request.method == "GET":
 		attending = modify_courses.attending(id, user_id)
 		if attending or admin:
-			return render_template("course.html", content = content, admin=admin, id = id, course = course, student_id = user_id, amount_answered = amount_answered, amount_tasks=amount_tasks, tasks = tasks)
+			return render_template("course.html", content = content, admin=admin, course = course, student_id = user_id, amount_answered = amount_answered, amount_tasks=amount_tasks, tasks = tasks)
 		else:
 			course = modify_courses.get_course(id)
 			return render_template("attend_course.html", course = course)
@@ -149,7 +157,7 @@ def course_site(id):
 			modify_courses.attend_course(id, user_id)
 			attending = modify_courses.attending(id, user_id)
 			if attending:
-				return render_template("course.html", content = content, admin=admin, id = id, course = course, student_id = user_id, amount_answered = amount_answered, amount_tasks=amount_tasks, tasks = tasks)
+				return render_template("course.html", content = content, admin=admin, course = course, student_id = user_id, amount_answered = amount_answered, amount_tasks=amount_tasks, tasks = tasks)
 			else:
 				return render_template("error.html", error="Kurssille ilmottautuminen ei onnistunut")
 
@@ -178,6 +186,7 @@ def question_site(id):
 						is_correct = "correctly"
 			all_choices = choices.get_all_choices(id)
 			answer = answers.find_answer(id, user_id, course_id)
+			
 			return render_template("question.html", task = task, all_choices=all_choices, answer = answer, course = course, choice_answer=choice_answer, correct_answer = correct_answer, is_correct = is_correct)
 
 
@@ -190,7 +199,7 @@ def students_attending_course():
 		course_id = request.form["course_id"]
 #		student_id = request.form["student_id"]
 		attending = modify_courses.all_students_in_course(course_id)
-		return render_template("students_attending_course.html", attending = attending)
+		return render_template("students_attending_course.html", attending = attending, course_id = course_id)
 
 @app.route("/modify_course", methods=["POST"])
 def modify_course():
@@ -251,7 +260,7 @@ def create_new_multiple_choice_question():
 			task_choices =  request.form.getlist("choice")
 			for choice in task_choices:
 				if choice != "":
-					choices.add_choice(task_id, choice, task_topic)
+					choices.add_choice(task_id, choice)
 			return redirect("/correct_answer/" + str(task_id))
 
 @app.route("/correct_answer/<int:id>", methods = ["GET", "POST"])
@@ -280,10 +289,11 @@ def add_choice_answer():
 	else:
 		task_id = request.form["task_id"]
 		student_id = session["user_id"]
+		course_id = request.form["course_id"]
 		if "answer" in request.form:
 			choice_id = request.form["answer"]
 			answers.add_choice_student_answer(task_id, choice_id, student_id)
-		return redirect("/courses")
+		return redirect("/courses/" + str(course_id))
 
 @app.route("/add_answer", methods=["POST"])
 def add_answer():
@@ -296,10 +306,9 @@ def add_answer():
 			content = request.form["content"]
 			if len(content) > 300  or len(content) <5:
 				return render_template("error.html", error="Answer is too long or short. Answer should be between 5-300 characters")
-			task_topic = request.form["topic"]
 			course_id = request.form["course_id"]
-			answers.add_answer(task_id, task_topic, student_id, content=content, course_id=course_id)
-			return redirect("/courses")
+			answers.add_answer(task_id, student_id, content=content, course_id=course_id)
+			return redirect("/courses/" + str(course_id))
  
 @app.route("/student_answers", methods = ["POST"])
 def student_answers():
@@ -310,9 +319,16 @@ def student_answers():
 		task_id = request.form["task_id"]
 		course_id = request.form["course_id"]
 		course = modify_courses.get_course(course_id)
-		all_answers = answers.get_all_task_answers(task_id)
-		return render_template("student_answers.html", all_answers = all_answers, course=course)
-	#puuttuu vielä monivalinta
+		task = modify_tasks.get_task(task_id)
+		all_answers = False
+		all_choice_answers = False
+		if task.multiple_choice == False:
+			all_answers = answers.get_all_task_answers(task_id)
+			return render_template("student_answers.html", task=task, all_answers = all_answers, all_choice_answers=all_choice_answers, course=course)
+		else:
+			all_choice_answers = answers.get_all_choice_answers(task_id)
+			return render_template("student_answers.html", task=task, all_choice_answers = all_choice_answers, course=course, all_answers = all_answers)
+
 
 @app.route("/delete_course/<int:id>")
 def delete_course_id(id):
